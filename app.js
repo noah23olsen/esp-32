be612eba005cd20910b69049671e66b2c5007796
Noah Olsen <noah23olsen@gmail.com>
2025-03-24 19:21:00 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+var state = "OFF";
+// Define a GET endpoint
+app.get("/", (req, res) => {
+  console.log(state, "Sent State");
+  res.send(state);
+});
+
+app.get("/change/:on", (req, res) => {
+  state = req.params.on == "ON" ? "ON" : "OFF";
+  res.send(state);
+});
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
